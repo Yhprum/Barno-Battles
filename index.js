fa2d337b8fff2e7ff9d943e23a44cd35e2b814ff
Yhprum <ryanmurphy@gatech.edu>
2017-08-02 09:00:08 -0700

    Started adding db support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require("path");
+var mongo = require("mongodb").MongoClient;
 
 var calculator = require("./calculator");
 
